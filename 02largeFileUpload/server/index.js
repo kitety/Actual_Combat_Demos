4ba68b998125566c88d10d4c6deb3fa31954acd8
刘正锟 <liuzhengkun@xiaoduotech.com>
2020-06-08 20:22:44 +0800

    添加合并

--- 02largeFileUpload/server/index.js
+++ 02largeFileUpload/server/index.js
@@ -5,6 +5,49 @@ const fse = require("fs-extra");
 const server = http.createServer();
 
 const UPLOAD_DIR = path.resolve(__dirname, "..", "target"); //大文件存储目录
+
+const resolvePost = (req) =>
+  new Promise((resolve) => {
+    let chunk = "";
+    req.on("data", (data) => {
+      chunk += data;
+    });
+    req.on("end", (data) => {
+      resolve(JSON.parse(chunk));
+    });
+  });
+
+const pipeStream = (path, writeStream) =>
+  new Promise((resolve) => {
+    const readSteam = fse.createReadStream(path);
+    readSteam.on("end", () => {
+      fse.unlinkSync(path);
+      resolve();
+    });
+    readSteam.pipe(writeStream);
+  });
+
+// 合并切片
+const mergeChunk = async (filePath, filename, size) => {
+  const chunkDir = path.resolve(UPLOAD_DIR, filename);
+  const chunkPaths = await fse.readdirSync(chunkDir);
+  // 根据下标进行排序
+  // 直接读取目录的顺序会错乱
+  chunkPaths.sort((a, b) => a.split("-")[1] - b.split("-")[1]);
+  await Promise.all(
+    chunkPaths.map((chunkPath, index) =>
+      pipeStream(
+        path.resolve(chunkDir, chunkPath),
+        // 指定位置创建读写流
+        fse.createWriteStream(filePath, {
+          start: index * size,
+          end: (index + 1) * size,
+        })
+      )
+    )
+  );
+  fse.rmdirSync(chunkDir);
+};
 server.on("request", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
