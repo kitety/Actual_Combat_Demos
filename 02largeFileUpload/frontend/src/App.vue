0079d7fd6fd270536eb2b2ea5fecb4586c1f8879
刘正锟 <liuzhengkun@xiaoduotech.com>
2020-06-10 21:08:37 +0800

    feat: 文件上传进度

--- 02largeFileUpload/frontend/src/App.vue
+++ 02largeFileUpload/frontend/src/App.vue
@@ -3,6 +3,28 @@
     <input type="file" @change="handleFileChange" />
     <el-button @click="handleUpload">上传</el-button>
     <el-button @click="merge">合并</el-button>
+    <div>
+      <div>总进度</div>
+      {{ uploadPercentage }}
+      <el-progress :percentage="uploadPercentage"></el-progress>
+    </div>
+    <el-table :data="data">
+      <el-table-column
+        prop="chunkHash"
+        label="切片hash"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        lable="大小"
+        align="center"
+        prop="size"
+      ></el-table-column>
+      <el-table-column label="进度" align="center">
+        <template v-slot="{ row }">
+          <el-progress :percentage="row.percentage"></el-progress>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -17,6 +39,17 @@ export default {
     },
     data: [],
   }),
+  computed: {
+    uploadPercentage() {
+      if (!this.container.file || !this.data.length) return 0;
+      console.log("this.data: ", this.data);
+      const loaded = this.data
+        .map((item) => item.chunk.size * item.percentage)
+        .reduce((acc, cur) => Number(acc) + Number(cur), 0);
+      console.log("loaded: ", loaded);
+      return parseInt((loaded / this.container.file.size).toFixed(2));
+    },
+  },
   methods: {
     handleFileChange(e) {
       console.log("e: ", e);
@@ -28,9 +61,16 @@ export default {
       this.container.file = file;
       console.log("file: ", file);
     },
-    request({ url, methods = "post", data, headers = {} }) {
+    request({
+      url,
+      methods = "post",
+      data,
+      headers = {},
+      onProgress = (e) => e,
+    }) {
       return new Promise((resolve) => {
         const xhr = new XMLHttpRequest();
+        xhr.upload.onprogress = onProgress;
         xhr.open(methods, url);
         Object.keys(headers).forEach((key) => {
           xhr.setRequestHeader(key, headers[key]);
@@ -57,17 +97,18 @@ export default {
     // 上传切片
     async uploadChunks() {
       const requestList = this.data
-        .map(({ chunk, hash }) => {
+        .map(({ chunk, hash, index }) => {
           const formData = new FormData();
           formData.append("chunk", chunk);
           formData.append("hash", hash);
           formData.append("filename", this.container.file.name);
-          return { formData };
+          return { formData, index };
         })
-        .map(async ({ formData }) => {
+        .map(async ({ formData, index }) => {
           this.request({
             url: "http://localhost:3000",
             data: formData,
+            onProgress: this.createProgressHandler(this.data[index]),
           });
         });
       // 并发切片
@@ -78,12 +119,21 @@ export default {
       if (!this.container.file) return;
       const fileChunkList = this.createFileChunk(this.container.file);
       console.log("fileChunkList: ", fileChunkList);
+      console.log("fileChunkList: ", fileChunkList);
       this.data = fileChunkList.map(({ file }, index) => ({
         chunk: file,
+        index,
         hash: this.container.file.name + "-" + index, // 文件名+数组小编
+        percentage: 0,
       }));
       await this.uploadChunks();
     },
+    createProgressHandler(item) {
+      return (e) => {
+        item.percentage = parseInt(String((e.loaded / e.total) * 100));
+      };
+    },
+
     async mergeRequest() {
       await this.request({
         url: "http://localhost:3000/merge",
