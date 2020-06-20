c05be58447c19a68111317e815444b65b3da946d
kitety <aizaizuori@foxmail.com>
2020-06-20 13:49:48 +0800

    feat: 开发和调试

---
+++ 03ui-labrary/components/alert/demo/1-demo-basic.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import Alert from 'happy-ui/alert';
+import 'happy-ui/lib/alert/style';
+
+export default () => <Alert kind="warning">11111111111</Alert>;
