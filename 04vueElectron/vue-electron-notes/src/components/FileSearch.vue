e2fbd0c8f4a9e101e446960466c760ff2ec153f8
刘正锟 <liuzhengkun@xiaoduotech.com>
2020-06-22 20:04:39 +0800

    feat: 新增文件

---
+++ 04vueElectron/vue-electron-notes/src/components/FileSearch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search-container">
+    <el-input placeholde="请输入内容" v-bind="$attrs" v-on="$listeners">
+      <el-button slot="append" icon="el-icon-search" />
+    </el-input>
+    <el-dropdown>
+      <el-button slot="primary" icon="el-icon-circle-plus-outline" circle />
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item @click="createFile()">新建笔记</el-dropdown-item>
+        <el-dropdown-item divided @click="importFile()">导入文件</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FileSearch',
+  methods: {
+    createFile() {
+      this.$emit('create')
+    },
+    importFile() {
+      this.$emit('import')
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>
